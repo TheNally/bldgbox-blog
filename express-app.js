a034a4bd2dfb6b34d7762b9825466c5a39413d85
NALLY <mike@theproductdump.com>
2014-09-28 01:35:08 -0400

    Update

--- express-app.js
+++ express-app.js
@@ -7,11 +7,11 @@ var app = module.exports = express();
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Hello World!!");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(5000);
 
 // Put a friendly message on the terminal
-console.log("Server running on port 8000!");
+console.log("Server running on port 5000!");
