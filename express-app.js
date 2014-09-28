c1cbb2e7c461b970c760e14c8a12ebf536b12f7d
NALLY <mike@theproductdump.com>
2014-09-28 01:25:17 -0400

    Bldgbox - First Commit

---
+++ express-app.js
@@ -0,0 +1,17 @@
+// Load required modules
+var http = require('http'),
+    express = require('express');
+
+var app = module.exports = express();
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running on port 8000!");
