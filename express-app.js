5d4efa27ad3313056f9defcd8bb7201998950829
NALLY <mike@theproductdump.com>
2014-09-28 03:32:44 -0400

    Commit

--- express-app.js
+++ express-app.js
@@ -1,6 +1,5 @@
 // Load required modules
-var http = require('http'),
-    express = require('express');
+var http = require('http');
 
 var app = module.exports = express();
 
@@ -10,7 +9,7 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World!!");
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
+// Listen on port 5000, IP defaults to 127.0.0.1
 server.listen(5000);
 
 // Put a friendly message on the terminal
